feeb0208c2ccb1cce1aaceb8c16df3dece442be6
Crowdbotics <team@crowdbotics.com>
2023-10-26 21:33:21 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "warriorperformancem-44171",
+  url: "https://warriorperformancem-44171.botics.co",
+  api: "https://warriorperformancem-44171.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
